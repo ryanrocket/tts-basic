5088974747c9399a2b1ef1431d16132cb249e284
ryanrocket <37909218+ryanrocket@users.noreply.github.com>
2019-01-22 19:32:05 -0500

    Update init.py

--- bin/init.py
+++ bin/init.py
@@ -1,7 +1,16 @@
 # tts-basic init Script
 
 # imports
-
-# import new files here
-
+import getter
 # end imports
+
+# globals
+global return_value
+global username
+global password
+# end globals
+
+# start Script
+print("TTS BASIC - Version 0.1.2 \nWould you like to sign in as a Teacher or Student?")
+getter.get()
+getter.login()
