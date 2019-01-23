6aef757f0e4488443bf277c2cd2d72ee2d4c1456
ryanrocket <37909218+ryanrocket@users.noreply.github.com>
2019-01-22 19:32:26 -0500

    Create getter.py

---
+++ bin/getter.py
@@ -0,0 +1,18 @@
+# Getter
+
+global login_type
+global return_value
+global username
+global password
+
+def get():
+    login_type = input("tts > ")
+    if (login_type == 'teacher' or login_type == 'Teacher' or login_type == 't'):
+        return_value = 1
+    elif (login_type == 'student' or login_type == 'Student' or login_type == 's'):
+        return_value = 2
+
+def login():
+    print("\nPlease enter your username and password:")
+    username = input("Username > ")
+    password = input("Password > ")
