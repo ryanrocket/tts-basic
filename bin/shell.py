9dbff3dca6981575bb0621327d994b00b2797e96
BuildTools <unconfigured@null.spigotmc.org>
2019-01-23 21:18:12 -0500

    Time for java :)

---
+++ bin/shell.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import datetime, time
+
+arg = sys.argv[1]
+
+# Condition Argument INPUT
+if (arg == "run"):
+    pass
+else :
+    print("please provide the 'run' argument to run!")
+
+# Start The Shell!
+def go():
+    given = input("tts > ")
+
+# STOP: ST GO JAVA
